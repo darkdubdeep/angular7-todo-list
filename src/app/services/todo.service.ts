026986222c72e67586102d42b4bbc466bbabe20d
darkdubdeep <darkdubdeep@gmail.com>
2019-02-28 23:09:19 +0400

    mock todos added

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,6 +1,7 @@
 import { Todo } from "./../models/Todo";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { of } from "rxjs";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 const httpOptions = {
@@ -16,10 +17,29 @@ export class TodoService {
   todosLimit: string = "?_limit=15";
   todosUrl: string = "https://jsonplaceholder.typicode.com/todos";
 
+  developMode: boolean = true;
+
+  mockTodos: Todo[] = [
+    {
+      id: 1,
+      title: "hello",
+      completed: true
+    },
+    {
+      id: 2,
+      title: "bye",
+      completed: false
+    }
+  ];
+
   constructor(private http: HttpClient) {}
 
   getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+    if (this.developMode === true) {
+      return of(this.mockTodos);
+    } else {
+      return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+    }
   }
 
   addTodo(todo: Todo): Observable<Todo> {
